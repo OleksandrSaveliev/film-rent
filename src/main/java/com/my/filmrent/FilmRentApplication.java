27f69a20b8c18595395158b519575c6f0e10a7af
OleksandrSaveliev <a.savelev.krm@gmail.com>
2026-04-29 03:06:06 +0300

    init commit

---
+++ src/main/java/com/my/filmrent/FilmRentApplication.java
@@ -0,0 +1,13 @@
+package com.my.filmrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmRentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmRentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/my/filmrent/FilmRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.filmrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmRentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
